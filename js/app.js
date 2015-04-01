5b1aababfba4a0f71d6f8bf21400756df0f11fd0
jukkamulla <jukkamulla@gmail.com>
2015-04-01 19:56:30 +0200

    first experience with angular

---
+++ js/app.js
@@ -0,0 +1,28 @@
+var fwtest = angular.module('fwtest', [
+    'ngRoute',
+    'fwtestControllers'
+]);
+
+fwtest.config(['$routeProvider',
+function($routeProvider) {
+    $routeProvider.
+        when('/welcome', {
+            templateUrl: './partials/welcome.html',
+            controller: 'welcomeCtrl'
+        }).
+        /*when('/game', {
+            templateUrl: './partials/game.html',
+            controller: 'gameCtrl'
+        }).
+        when('/win', {
+            templateUrl: './partials/win.html',
+            controller: 'winCtrl'
+        }).
+        when('/lose', {
+            templateUrl: './partials/lose.html',
+            controller: 'loseCtrl'
+        }). */
+        otherwise({
+            redirectTo: '/welcome'
+        });
+}]);
---
+++ js/controllers.js
@@ -0,0 +1,60 @@
+var fwtestControllers = angular.module('fwtestControllers', []);
+
+fwtestControllers.controller('welcomeCtrl', [
+    '$scope',
+    '$location',
+    function ($scope, $location) {
+        $scope.name = 'Muci';
+        $scope.play = function ( path ) {
+            console.log('go to play');
+            $location.path( path );
+        };
+    }
+]);
+
+/*fwtestControllers.controller('gameCtrl', [
+    '$scope',
+    'gamestate',
+    '$location',
+    function ($scope, gamestate, $location) {
+        $scope.game = 'GAME';
+        $scope.state = gamestate;
+
+        $scope.lose = function () {
+            $location.path( '/lose' );
+        };
+
+        $scope.win = function () {
+            gamestate.levelUp();
+            $location.path( '/win' );
+        };
+
+        $scope.home = function () {
+            $location.path( '/home' );
+        };
+    }
+]);
+
+fwtestControllers.controller('winCtrl', [
+    '$scope',
+    'gamestate',
+    '$location',
+    function ($scope, gamestate, $location) {
+        $scope.state = gamestate;
+        $scope.back = function () {
+            $location.path( '/game' );
+        };
+    }
+]);
+
+fwtestControllers.controller('loseCtrl', [
+    '$scope',
+    'gamestate',
+    '$location',
+    function ($scope, gamestate, $location) {
+        $scope.state = gamestate;
+        $scope.back = function () {
+            $location.path( '/game' );
+        };
+    }
+]); */
